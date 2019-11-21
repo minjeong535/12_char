48e038906cf5c8a219896979c43b5105ba43ebb1
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-11-21 15:21:04 +0900

    05

--- main.c
+++ main.c
@@ -4,11 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
-	
-    strcpy(dst, src);
-    
-    printf("문자열\"%s\"의 길이 :%d", dst, strlen(dst));
+ int i;
+ char input[100];
+ FILE *fp;
+ 
+ fp = fopen("sample.txt", "w");
+ for(i=0; i<3; i++){
+ 	printf("input a word : ");
+ 	scanf("%s", input);
+ 	fprintf(fp, "%s\n", input);
+ 	
+ }
+ fclose(fp);
 	return 0;
 }
